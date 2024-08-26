07798f1a89a181eb735b514f8c5108a1785a0f79
pinjaa <91983754+pinjaa@users.noreply.github.com>
2024-08-26 09:28:27 +0300

    käyttäjän rekisteröinti

---
+++ macromenu/components/Register.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import {Text, View, Pressable, Alert } from 'react-native';
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const checkInformation = () => {
+    if(!email) {
+      Alert.alert('Email is required');
+    }
+    if(!username) {
+      Alert.alert('Username is required');
+    }
+    if(!password) {
+      Alert.alert('Password is required');
+    }
+    if(!confirmPassword) {
+      Alert.alert('Confirm password!');
+    }
+    if(confirmPassword !== password) {
+      Alert.alert('Passwords do not match!');
+    }
+  }
+
+  return (
+    <View>
+      <Text>Register</Text>
+      <TextInput 
+        placeholder='Enter your email'
+        value={email}
+        onChangeText={(email) => setEmail(email.trim())} 
+      />
+      <TextInput 
+        placeholder='Enter a username'
+        value={username}
+        onChangeText={(username) => setUsername(username.trim())} 
+      />
+      <TextInput 
+        placeholder='Enter a password'
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        secureTextEntry={true}
+      />
+      <TextInput 
+        placeholder='Confirm password'
+        value={confirmPassword}
+        onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+        secureTextEntry={true}
+      />
+
+      <View style={[{flex: 1},{alignItems:'center'}]}>
+        <Pressable onPress={() => checkInformation()}>
+          <Text>REGISTER</Text>
+        </Pressable>
+        <Text>Already have an account?</Text>
+        <Pressable
+          onPress={() => navigation.navigate('Login')}
+        >
+          <Text>LOGIN</Text>
+        </Pressable>
+      </View>
+    </View>
+    
+  )
+}
